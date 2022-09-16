85854e519ab4f0710327fee4cc40aee325b07bbe
Rohan Ramakrishnan <rohanramakrish@gmail.com>
2022-09-15 19:42:19 -0700

    add utility method isBase32

--- mod.ts
+++ mod.ts
@@ -4,3 +4,4 @@ export { isEmail } from "./lib/isEmail.ts";
 export { equals } from "./lib/equals.ts";
 export { contains } from "./lib/contains.ts";
 export { matches } from "./lib/matches.ts";
+export { isBase32 } from "./lib/isBase32.ts";
